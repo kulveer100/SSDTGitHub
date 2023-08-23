a466954a24ba5c660b3f82b0369af6b666589510
Kulveer Manshahia <kulveerkaur323@gmail.com>
2023-08-23 14:23:44 -0400

    Date added column

--- SSDTGitHub/Test.sql
+++ SSDTGitHub/Test.sql
@@ -2,5 +2,6 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [FirstName] NVARCHAR(50) NULL, 
-    [LastName] NVARCHAR(50) NULL
+    [LastName] NVARCHAR(50) NULL, 
+    [DateAdded] DATETIME NULL DEFAULT GETDATE()
 )
